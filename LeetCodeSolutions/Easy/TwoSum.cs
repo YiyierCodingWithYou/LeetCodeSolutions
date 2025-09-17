ca7efa7cc4b69870de32b31119f7557ae47e3e01
Yiyier <iamkellyoung@gmail.com>
2025-09-18 00:33:55 +0800

    Rename LeetCode1 to TwoSum and add alternative solution
    - Rename file/class from LeetCode1 to TwoSum to match the problem name
    - Implement a new approach for TwoSum using a dictionary for O(n) solution
    - Add comments

---
+++ LeetCodeSolutions/Easy/TwoSum.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+
+namespace LeetCodeSolutions.Easy
+{
+	public class TwoSum
+	{
+		/// <summary>
+		/// 雙重迴圈的解法
+		/// </summary>
+		/// <param name="nums"></param>
+		/// <param name="target"></param>
+		/// <returns></returns>
+		public int[] TwoSumA(int[] nums, int target)
+		{
+			int[] result = new int[2];
+			for (int i = 0; i <= nums.Length - 1; i++)
+			{
+				for (int j = i + 1; j <= nums.Length - 1; j++)
+					if ((nums[i] + nums[j]) == target)
+					{
+						result[0] = i;
+						result[1] = j;
+						break;
+					}
+			}
+			return result;
+		}
+
+		/// <summary>
+		/// 只需要一次迴圈的解法
+		/// </summary>
+		/// <param name="nums"></param>
+		/// <param name="target"></param>
+		/// <returns></returns>
+		public int[] TwoSumB(int[] nums, int target)
+		{
+
+			Dictionary<int,int> storedNum = new Dictionary<int,int>();
+			for (int i = 0; i < nums.Length; i++)
+			{
+				var neededNum = target - nums[i];
+
+				if (storedNum.TryGetValue(neededNum,out int index))
+				{
+					return new int[] { index, i };
+				}
+				else
+				{
+					if (!storedNum.ContainsKey(nums[i]))
+					{
+						storedNum.Add(nums[i], i);
+					}
+				}
+			}
+
+			return Array.Empty<int>();
+		}
+
+	}
+}
