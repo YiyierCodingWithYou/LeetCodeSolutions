0791be659915803f474212bdaf8d9adde4a5c5eb
Yiyier <iamkellyoung@gmail.com>
2025-09-19 18:02:47 +0800

    implement FindLongestCommonPrefix using vertical scan
    - Uses shortest string length as limit to avoid index out of range
    - Iterates character by character and compares across all strings

---
+++ LeetCodeSolutions/Easy/LongestCommonPrefix.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+	public class LongestCommonPrefix
+	{
+		/// <summary>
+		/// 從傳入的字串陣列中找出最長的前綴詞
+		/// </summary>
+		/// <param name="strs"></param>
+		/// <returns></returns>
+		public string FindLongestCommonPrefix(string[] strs)
+		{
+			var result = "";
+
+			if (strs == null || strs.Length == 0)
+			{
+				return result;
+			}
+
+			// 先找出最短單字的字數
+			var shortestStrLength = strs.Min(x => x.Length);
+
+			for (int i = 0; i < shortestStrLength; i++)
+			{
+				char baseChar = strs[0][i];
+
+				for (int j = 0; j < strs.Length; j++)
+				{
+					if (strs[j][i] != baseChar)
+					{
+						return result;
+					}
+				}
+
+				result += baseChar;
+			}
+
+			return result;
+		}
+	}
+}
