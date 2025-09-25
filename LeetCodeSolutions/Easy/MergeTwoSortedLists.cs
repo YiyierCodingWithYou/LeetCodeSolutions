9af221bae66d2a27bc5b4da155e3f99dce2f2da8
Yiyier <iamkellyoung@gmail.com>
2025-09-25 22:31:36 +0800

    implement MergeTwoLists
    - Traverse list1 and list2, collect values into intList
    - Sort values and rebuild a new sorted linked list

---
+++ LeetCodeSolutions/Easy/MergeTwoSortedLists.cs
@@ -0,0 +1,48 @@
+﻿using LeetCodeSolutions.DataStructures;
+
+namespace LeetCodeSolutions.Easy
+{
+	public class MergeTwoSortedLists
+	{
+		/// <summary>
+		/// 合併兩個已排序的ListNode並回傳新的排序後ListNode
+		/// </summary>
+		/// <param name="list1"></param>
+		/// <param name="list2"></param>
+		/// <returns></returns>
+		public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+		{
+			var result = new ListNode(0);
+			var current = result;
+			var intList = new List<int>();
+
+			while (list1 != null || list2 != null)
+			{
+				if (list1 != null)
+				{
+					intList.Add(list1.val);
+					list1 = list1.next;
+				}
+
+				if (list2 != null)
+				{
+					intList.Add(list2.val);
+					list2 = list2.next;
+				}
+			}
+
+			intList.Sort();
+
+			if(intList.Count > 0)
+			{
+				for(int i = 0; i < intList.Count; i++)
+				{
+					current.next = new ListNode(intList[i]);
+					current = current.next;
+				}
+			}
+
+			return result.next;
+		}
+	}
+}
