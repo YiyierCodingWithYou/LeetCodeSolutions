98cc5141aefdda223db998009ac3eae238178006
Yiyier <iamkellyoung@gmail.com>
2025-09-25 22:43:49 +0800

    add MergeTwoListsB
    - Implement in-place merge by comparing list1 and list2 nodes
    - Attach remaining nodes directly without extra sorting
    - Keep original MergeTwoLists as MergeTwoListsA

--- LeetCodeSolutions/Easy/MergeTwoSortedLists.cs
+++ LeetCodeSolutions/Easy/MergeTwoSortedLists.cs
@@ -10,7 +10,7 @@ namespace LeetCodeSolutions.Easy
 		/// <param name="list1"></param>
 		/// <param name="list2"></param>
 		/// <returns></returns>
-		public ListNode MergeTwoLists(ListNode list1, ListNode list2)
+		public ListNode MergeTwoListsA(ListNode list1, ListNode list2)
 		{
 			var result = new ListNode(0);
 			var current = result;
@@ -40,6 +40,40 @@ namespace LeetCodeSolutions.Easy
 					current.next = new ListNode(intList[i]);
 					current = current.next;
 				}
+			}
+
+			return result.next;
+		}
+
+		public ListNode MergeTwoListsB(ListNode list1, ListNode list2)
+		{
+			var result = new ListNode(0);
+			var current = result;
+
+			while (list1 != null && list2 != null)
+			{
+				if (list2.val >= list1.val)
+				{
+					current.next = new ListNode(list1.val);
+					list1 = list1.next;
+				}
+				else
+				{
+					current.next = new ListNode(list2.val);
+					list2 = list2.next;
+				}
+
+				current = current.next;
+			}
+
+			if(list1 != null && list2 == null)
+			{
+				current.next = list1;
+			}
+
+			if(list1 == null && list2 != null)
+			{
+				current.next = list2;
 			}
 
 			return result.next;
