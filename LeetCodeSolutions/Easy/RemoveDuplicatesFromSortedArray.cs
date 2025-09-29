05e7d7c1e8530351ab2d79a11377c3d95fb2fe47
Yiyier <iamkellyoung@gmail.com>
2025-09-30 02:26:23 +0800

    implement RemoveDuplicates
    - Added method to remove duplicates from sorted array in-place

---
+++ LeetCodeSolutions/Easy/RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+	public class RemoveDuplicatesFromSortedArray
+	{
+		/// <summary>
+		/// 移除陣列中重複的數字，並回傳不重複數字的個數
+		/// </summary>
+		/// <param name="nums"></param>
+		/// <returns></returns>
+		public int RemoveDuplicates(int[] nums)
+		{
+			if (nums.Length <= 0)
+			{
+				return 0;
+			}
+
+			int amount = 1;
+
+			for (int i = 1; i < nums.Length; i++)
+			{
+				if (nums[i] != nums[i - 1])
+				{
+					nums[amount] = nums[i];
+					amount++;
+				}
+			}
+
+			return amount;
+		}
+	}
+}
