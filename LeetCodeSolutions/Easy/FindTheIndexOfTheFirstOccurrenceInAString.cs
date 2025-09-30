2994014fd530bed9099a5920778f29628295f354
Yiyier <iamkellyoung@gmail.com>
2025-09-30 15:27:38 +0800

    add FindTheIndexOfTheFirstOccurrenceInAString
    - Implemented StrStr method to find the first occurrence of a substring in a string
    - Used substring comparison with sliding window loop

---
+++ LeetCodeSolutions/Easy/FindTheIndexOfTheFirstOccurrenceInAString.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+	public class FindTheIndexOfTheFirstOccurrenceInAString
+	{
+		/// <summary>
+		/// 在 haystack 字串中尋找目標字串 needle，回傳第一次出現的位置索引，若不存在則回傳 -1。
+		/// </summary>
+		/// <param name="haystack"></param>
+		/// <param name="needle"></param>
+		/// <returns></returns>
+		public int StrStr(string haystack, string needle)
+		{
+			for (int i = 0; i <= haystack.Length - needle.Length; i++)
+			{
+				if(haystack.Substring(i,needle.Length) == needle)
+				{
+					return i;
+				}
+			}
+
+			return -1;
+		}
+	}
+}
