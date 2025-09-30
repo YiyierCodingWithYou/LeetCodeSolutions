c4ae89ea1aa2db5132e222a592409caa325386be
Yiyier <iamkellyoung@gmail.com>
2025-09-30 15:00:08 +0800

    implement RemoveElementFromArray
    - Added method to remove all occurrences of a target value from an array in-place
    - Used two-pointer technique to overwrite valid elements

---
+++ LeetCodeSolutions/Easy/RemoveElement.cs
@@ -0,0 +1,32 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+	public class RemoveElement
+	{
+		/// <summary>
+		/// 從陣列中移除指定數字
+		/// </summary>
+		/// <param name="nums"></param>
+		/// <param name="val"></param>
+		/// <returns></returns>
+		public int RemoveElementFromArray(int[] nums, int val)
+		{
+			if(nums.Length <= 0)
+			{
+				return 0;
+			}
+
+			int amount = 0;
+
+			for(int i = 0; i < nums.Length; i++)
+			{
+				if (nums[i] != val)
+				{
+					nums[amount] = nums[i];
+					amount++;
+				}
+			}
+
+			return amount;
+		}
+	}
+}
