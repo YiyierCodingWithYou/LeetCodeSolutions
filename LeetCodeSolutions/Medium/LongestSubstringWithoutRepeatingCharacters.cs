887d5474614ece1088d584ccd5b88ae4b0cd04a9
Yiyier <iamkellyoung@gmail.com>
2025-10-02 01:25:31 +0800

    implement LengthOfLongestSubstring using sliding window
    - Added two-pointer approach (left, right) with HashSet to track characters
    - Updated length calculation using if-condition

---
+++ LeetCodeSolutions/Medium/LongestSubstringWithoutRepeatingCharacters.cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeSolutions.Medium
+{
+	public class LongestSubstringWithoutRepeatingCharacters
+	{
+		/// <summary>
+		/// 找出不含重複字元的最長子字串的長度。
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		public int LengthOfLongestSubstring(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+			{
+				return 0;
+			}
+
+			var length = 0;
+			int left = 0, right = 0;
+			HashSet<char> temp = new HashSet<char>();
+
+			while (right < s.Length)
+			{
+				if (!temp.Contains(s[right]))
+				{
+					temp.Add(s[right]);
+					right++;
+
+					if((right - left) > length)
+					{
+						length = right - left;
+					}
+				}
+				else
+				{
+					temp.Remove(s[left]);
+					left++;
+				}
+			}
+
+			return length;
+		}
+	}
+}
