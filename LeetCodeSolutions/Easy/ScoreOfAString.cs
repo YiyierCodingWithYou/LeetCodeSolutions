686a0b195cae94f4f8f01eb32d02546a391f2cd9
Yiyier <iamkellyoung@gmail.com>
2025-10-02 15:51:59 +0800

    add ScoreOfAString (LeetCode 3110)
    - Implemented method to calculate score of a string
    - Score is defined as sum of absolute ASCII differences between adjacent characters

---
+++ LeetCodeSolutions/Easy/ScoreOfAString.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+	/// <summary>
+	/// LeetCode_3110
+	/// </summary>
+	public class ScoreOfAString
+	{
+		/// <summary>
+		/// 求出相鄰字元的ASCII值差的絕對值總和。
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		public int ScoreOfString(string s)
+		{
+			if (string.IsNullOrEmpty(s))
+			{
+				return 0;
+			}
+
+			var sum = 0;
+
+			for (int i = 0; i < s.Length - 1; i++)
+			{
+				sum += Math.Abs((int)s[i] - (int)s[i + 1]);
+			}
+
+			return sum;
+		}
+	}
+}
