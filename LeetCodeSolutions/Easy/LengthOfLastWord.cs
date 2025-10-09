87f34594126a00981b3bb89bf7043f9cdc469f13
Yiyier <iamkellyoung@gmail.com>
2025-10-09 15:30:37 +0800

    implement LengthOfLastWord
    - Added method to return the length of the last word in a string
    - Uses Trim and Split to handle spaces

---
+++ LeetCodeSolutions/Easy/LengthOfLastWord.cs
@@ -0,0 +1,19 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+	/// <summary>
+	/// LeetCode_58
+	/// </summary>
+	public class LengthOfLastWord
+	{
+		/// <summary>
+		/// 回傳字串中最後一個單字的長度。
+		/// </summary>
+		/// <param name="s"></param>
+		/// <returns></returns>
+		public int FindLengthOfLastWord(string s)
+		{
+			var strArray = s.Trim().Split(' ');
+			return strArray[strArray.Length - 1].Length;
+		}
+	}
+}
