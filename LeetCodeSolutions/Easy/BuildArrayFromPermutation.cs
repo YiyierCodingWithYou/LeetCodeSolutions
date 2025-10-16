ebe01525655be7fd56d7ce4de568c2ac83425d17
Yiyier <iamkellyoung@gmail.com>
2025-10-16 18:52:15 +0800

    implement BuildArrayFromPermutation
    - Added BuildArray method to construct an array where each element is nums[nums[i]]
    - Uses a new array to store results

---
+++ LeetCodeSolutions/Easy/BuildArrayFromPermutation.cs
@@ -0,0 +1,25 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+	/// <summary>
+	/// LeetCode_1920
+	/// </summary>
+	public class BuildArrayFromPermutation
+	{
+		/// <summary>
+		/// 建立一個陣列，使每個位置的值等於原陣列中該位置值所指向位置的數字。
+		/// </summary>
+		/// <param name="nums"></param>
+		/// <returns></returns>
+		public int[] BuildArray(int[] nums)
+		{
+			int[] result = new int[nums.Length];
+
+			for(int i = 0; i < nums.Length; i++)
+			{
+				result[i] = nums[nums[i]];
+			}
+
+			return result;
+		}
+	}
+}
