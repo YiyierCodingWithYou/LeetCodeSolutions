57c8a890ccaf3362849116cea791c4b240b49be6
Yiyier <iamkellyoung@gmail.com>
2025-10-16 18:28:02 +0800

    implement DifferenceOfSums
    - Added DifferenceOfSums method to calculate the difference between sums of numbers not divisible by m and numbers divisible by m in range [1, n]
    - Uses simple loop to separate divisible and non-divisible numbers

---
+++ LeetCodeSolutions/Easy/DivisibleAndNonDivisibleSumsDifference.cs
@@ -0,0 +1,33 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+	/// <summary>
+	/// LeetCode_2894
+	/// </summary>
+	public class DivisibleAndNonDivisibleSumsDifference
+	{
+		/// <summary>
+		/// 計算區間 [1, n] 中，所有不能被 m 整除的數字總和與能被 m 整除的數字總和之差。
+		/// </summary>
+		/// <param name="n"></param>
+		/// <param name="m"></param>
+		/// <returns></returns>
+		public int DifferenceOfSums(int n, int m)
+		{
+			int num1 = 0, num2 = 0;
+			
+			for(int i = 1; i <= n; i++)
+			{
+				if(i % m == 0)
+				{
+					num2 += i;
+				}
+				else
+				{
+					num1 += i;
+				}
+			}
+
+			return num1 - num2;
+		}
+	}
+}
