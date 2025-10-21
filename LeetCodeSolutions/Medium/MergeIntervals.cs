3f3e68ff9fe2f9f8b59896414b264c1170855ec1
Yiyier <iamkellyoung@gmail.com>
2025-10-21 17:25:19 +0800

    refactor: rename Merge to MergeA
    - Renamed original Merge method to MergeA for clarity
    - Added new optimized version MergeB with improved merge conditions

--- LeetCodeSolutions/Medium/MergeIntervals.cs
+++ LeetCodeSolutions/Medium/MergeIntervals.cs
@@ -10,12 +10,12 @@
 		/// </summary>
 		/// <param name="intervals"></param>
 		/// <returns></returns>
-		public int[][] Merge(int[][] intervals)
+		public int[][] MergeA(int[][] intervals)
 		{
 			intervals = intervals.OrderBy(x => x[0]).ToArray();
 
 			List<int[]> result = new List<int[]>() { intervals[0] };
-			
+
 			var start = intervals[0][0];
 			var end = intervals[0][1];
 
@@ -36,5 +36,37 @@
 
 			return result.ToArray();
 		}
+
+		/// <summary>
+		/// 效能、程式碼優化
+		/// </summary>
+		/// <param name="intervals"></param>
+		/// <returns></returns>
+		public int[][] MergeB(int[][] intervals)
+		{
+			Array.Sort(intervals, (num1, num2) => num1[0].CompareTo(num2[0]));
+
+			List<int[]> result = new List<int[]>();
+
+			var current = intervals[0];
+
+			for (int i = 1; i < intervals.Length; i++)
+			{
+				if (intervals[i][0] <= current[1])
+				{
+					current[1] = current[1] > intervals[i][1] ? current[1] : intervals[i][1];
+				}
+				else
+				{
+					result.Add(current);
+					current = intervals[i];
+				}
+			}
+
+			result.Add(current);
+
+			return result.ToArray();
+		}
+
 	}
 }
