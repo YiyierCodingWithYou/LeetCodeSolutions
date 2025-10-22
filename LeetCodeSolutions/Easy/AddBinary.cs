8c2f85ccf36d2ce4a4ce4c7da926cce5cc7347b8
Yiyier <iamkellyoung@gmail.com>
2025-10-23 03:37:06 +0800

    implement AddBinary
    - Added GetBinarySum method to add two binary strings and return their sum as a binary string
    - Used StringBuilder to efficiently append digits and reversed the result for correct order

---
+++ LeetCodeSolutions/Easy/AddBinary.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace LeetCodeSolutions.Easy
+{
+	/// <summary>
+	/// LeetCode_67
+	/// </summary>
+	public class AddBinary
+	{
+		/// <summary>
+		/// 給定兩個二進位字串 a 和 b，回傳它們相加後的二進位結果字串。
+		/// </summary>
+		/// <param name="a"></param>
+		/// <param name="b"></param>
+		/// <returns></returns>
+		public string GetBinarySum(string a, string b)
+		{
+			var temp = new StringBuilder();
+			var carry = 0;
+			var aIdx = a.Length - 1;
+			var bIdx = b.Length - 1;
+
+			while (aIdx >= 0 || bIdx >= 0 || carry != 0)
+			{
+				var aNum = 0;
+				var bNum = 0;
+
+				if (aIdx >= 0)
+				{
+					aNum = a[aIdx] - '0';
+				}
+
+				if (bIdx >= 0)
+				{
+					bNum = b[bIdx] - '0';
+				}
+
+				var sum = aNum + bNum + carry;
+
+				carry = sum / 2;
+
+				temp.Append(sum % 2);
+
+				aIdx--;
+				bIdx--;
+			}
+
+			var result = temp.ToString().ToCharArray();
+			Array.Reverse(result);
+
+			return new string(result);
+		}
+	}
+}
