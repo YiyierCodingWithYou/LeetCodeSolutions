f690b32f99a67c294dbbdb680edc13e4cfaf6a6a
Yiyier <iamkellyoung@gmail.com>
2025-10-23 18:06:35 +0800

    implement integer square root
    - Added binary search method to compute floor of square root without using Math functions
    - Handled overflow by casting mid to long during multiplication

---
+++ LeetCodeSolutions/Easy/Sqrt(x).cs
@@ -0,0 +1,43 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+	/// <summary>
+	/// LeetCode_69
+	/// </summary>
+	public class Sqrt_x_
+	{
+		/// <summary>
+		/// 找出平方根的最小整數
+		/// </summary>
+		/// <param name="x"></param>
+		/// <returns></returns>
+		public int MySqrt(int x)
+		{
+			if (x == 0 || x == 1)
+			{
+				return x;
+			}
+
+			var result = 0;
+			var start = 0;
+			var end = x;
+
+			while (start <= end)
+			{
+				int mid = (start + end) / 2;
+
+				// 避免溢位
+				if ((long)mid * mid <= x)
+				{
+					result = mid;
+					start = mid + 1;
+				}
+				else
+				{
+					end = mid - 1;
+				}
+			}
+
+			return result;
+		}
+	}
+}
