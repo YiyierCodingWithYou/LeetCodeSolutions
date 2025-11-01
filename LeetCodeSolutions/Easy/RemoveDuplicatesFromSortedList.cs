ca6b0e344f55c93c5c6bbbb155dfb2c2a730401a
Yiyier <iamkellyoung@gmail.com>
2025-11-01 15:12:22 +0800

    implement RemoveDuplicatesFromSortedList
    - Added method to remove duplicates from a sorted linked list

---
+++ LeetCodeSolutions/Easy/RemoveDuplicatesFromSortedList.cs
@@ -0,0 +1,34 @@
+﻿using LeetCodeSolutions.DataStructures;
+
+namespace LeetCodeSolutions.Easy
+{
+	/// <summary>
+	/// LeetCode_83
+	/// </summary>
+	public class RemoveDuplicatesFromSortedList
+	{
+		/// <summary>
+		/// 於 linked list 中刪除重複的項目
+		/// </summary>
+		/// <param name="head"></param>
+		/// <returns></returns>
+		public ListNode DeleteDuplicates(ListNode head)
+		{
+			var current = head;
+			
+			while (current != null && current.next !=null)
+			{
+				if(current.val == current.next.val)
+				{
+					current.next = current.next.next;
+				}
+				else
+				{
+					current = current.next;
+				}
+			}
+
+			return head;
+		}
+	}
+}
