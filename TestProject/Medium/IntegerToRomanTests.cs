7fc1b4ebf6203889bce12bea72101f1e70508f71
Yiyier <iamkellyoung@gmail.com>
2025-11-06 16:43:15 +0800

    Add NUnit test project and IntegerToRomanTests
    - Created new TestProject using NUnit framework
    - Added IntegerToRomanTests with multiple [TestCase] validations for IntToRoman conversion

---
+++ TestProject/Medium/IntegerToRomanTests.cs
@@ -0,0 +1,26 @@
+﻿using LeetCodeSolutions.Medium;
+
+namespace TestProject.Medium
+{
+	[TestFixture]
+	public class IntegerToRomanTests
+	{
+		private IntegerToRoman _converter;
+
+		[SetUp]
+		public void Setup()
+		{
+			_converter = new IntegerToRoman();
+		}
+
+		[TestCase(3749, "MMMDCCXLIX")]
+		[TestCase(58, "LVIII")]
+		[TestCase(1994, "MCMXCIV")]
+
+		public void IntToRoman_ReturnsExpectedResult(int num, string expected)
+		{
+			var result = _converter.IntToRoman(num);
+			Assert.That(result, Is.EqualTo(expected));
+		}
+	}
+}
