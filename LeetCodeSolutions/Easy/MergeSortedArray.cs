4cb52e4c86ba73bffb44884ef57a52b7977a2787
Yiyier <iamkellyoung@gmail.com>
2025-11-08 18:19:53 +0800

    implement MergeSortedArray with unit tests
    - Added MergeSortedArray class to merge two sorted integer arrays into nums1
    - Implemented handling for m = 0 using Array.Copy
    - Added NUnit test cases in MergeSortedArrayTests to validate merge correctness across different scenarios

---
+++ LeetCodeSolutions/Easy/MergeSortedArray.cs
@@ -0,0 +1,31 @@
+﻿namespace LeetCodeSolutions.Easy
+{
+	/// <summary>
+	/// LeetCode_88
+	/// </summary>
+	public class MergeSortedArray
+	{
+		/// <summary>
+		/// 將 nums2 陣列合併至 nums1 陣列中，並按照由小到大的順序排列
+		/// </summary>
+		/// <param name="nums1"></param>
+		/// <param name="m"></param>
+		/// <param name="nums2"></param>
+		/// <param name="n"></param>
+		public void Merge(int[] nums1, int m, int[] nums2, int n)
+		{
+			if (m == 0)
+			{
+				Array.Copy(nums2, nums1, n);
+				return;
+			}
+
+			for (int i = m; i < m + n; i++)
+			{
+				nums1[i] = nums2[i - m];
+			}
+
+			Array.Sort(nums1);
+		}
+	}
+}
---
+++ TestProject/Easy/MergeSortedArrayTests.cs
@@ -0,0 +1,27 @@
+﻿using LeetCodeSolutions.Easy;
+
+namespace TestProject.Easy
+{
+	[TestFixture]
+	public class MergeSortedArrayTests
+	{
+		private MergeSortedArray _converter;
+
+		[SetUp]
+		public void Setup()
+		{
+			_converter = new MergeSortedArray();
+		}
+
+		[TestCase(new int[] { 1, 2, 3, 0, 0, 0 }, 3, new int[] { 2, 5, 6 }, 3, new int[] { 1, 2, 2, 3, 5, 6 })]
+		[TestCase(new int[] { 1 }, 1, new int[] { 0 }, 1, new int[] { 1 })]
+		[TestCase(new int[] { 0 }, 0, new int[] { 1 }, 1, new int[] { 1 })]
+
+
+		public void MergeSortedArray_ReturnsExpectedResult(int[] nums1, int m, int[] nums2, int n , int[] expected)
+		{
+			_converter.Merge(nums1, m , nums2, n);
+			Assert.That(nums1, Is.EqualTo(expected));
+		}
+	}
+}
